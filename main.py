6dd3a2f3016097d7a539c70b0895729aa066af12
AlanSaid1 <marti2002_@hotmail.com>
2024-03-16 16:56:58 -0600

    creacion de clase fruta

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-# Example file showing a circle moving on screen
-import pygame
+import pygame,sys
+from pygame.math import Vector2
 
-# pygame setup
+class Fruit:
+    # The position of the fruit
+    def __init__(self):
+        self.x = 4
+        self.y = 5
+        self.position = Vector2(self.x, self.y)
+
+    # Draw the fruit
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(self.position.x, self.position.y, cell_size, cell_size)
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
+
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-running = True
 dt = 0
-
+cell_size = 40
 player_pos = pygame.rect.Rect(100, 100, 40, 40)
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+fruit = Fruit()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    screen.fill('gray')
 
     pygame.draw.rect(screen, "blue", player_pos, 40)
 
@@ -32,12 +42,6 @@ while running:
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
+    fruit.draw_fruit()
+    pygame.display.update()
     dt = clock.tick(60) / 1000
-
-pygame.quit()
